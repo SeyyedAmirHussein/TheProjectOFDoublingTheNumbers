fb70f3d906486213893f8dca90e6145ce5e8fe31
SeyyedAmirHussein <148333552+SeyyedAmirHussein@users.noreply.github.com>
2023-11-09 21:40:59 +0330

    Create The Main Code OF The Project OF Doubling The Numbers . .C

---
+++ The Main Code OF The Project OF Doubling The Numbers . .C
@@ -0,0 +1,35 @@
+// The Project OF Doubling The Numbers .
+
+// The Main Code OF The Project OF Doubling The Numbers . .C
+
+// In this program, We want to Receive a Number from the Serial and then print Double that on the Serial Monitor .
+
+
+
+
+void setup () {
+
+  Serial.begin (9600);                                                                                                                              // Introducing The Series .
+
+}
+
+
+void loop () {
+
+
+  if (Serial.available () > 0 ) {                                                                                                         //
+
+    int The Number = Serial.parseInt ();                                                                                                //
+    int The Double OF The Number = The Number * 2;                                                                                     //
+
+
+    Serial.print (" * * * * The Double OF The Number : * * * * * ");                                                               // Show The Desired Message .
+    Serial.print (The Number);                                                                                                    // Show The Desired Message .
+    Serial.print (" * * * * Is : * * * * ");                                                                                     // Show The Desired Message .
+    Serial.print (The Double OF The Number);                                                                                    // Show The Desired Message .
+
+
+    }
+
+
+}
